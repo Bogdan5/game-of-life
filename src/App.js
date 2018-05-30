2f1d7fe2e9e13498111f4f905e808b0d57eec7ce
Bogdan5 <bogdan5x@gmail.com>
2018-05-29 20:54:51 -0400

    'Comment'

--- src/App.js
+++ src/App.js
@@ -9,55 +9,48 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      boardWidth: 7,
-      boardHeight: 7,
+      boardWidth: 40,
+      boardHeight: 30,
       board: [],
       number: 12,
       start: false,
       paused: false,
       reset: false,
       speed: 600,
+      boardSize: 'Small',
     };
   }
 
-  // componentDidMount() {
-  //   this.setState({ number: Math.floor(this.state.boardWidth * this.state.boardHeight / 2) });
-  // }
-
   clicked = (type) => {
-    let obj = {};
-    if (type === 'start') {
+    if (type === 'Start') {
       this.setState({ start: true, paused: false, reset: false, });
-    } else if (type === 'paused') {
+    } else if (type === 'Pause') {
       const p = !this.state.paused;
-      console.log('paused clicked', p);
       this.setState({ start: false, paused: p, reset: false, });
-    } else if (type === 'reset') {
+    } else if (type === 'Reset') {
       this.setState({ start: false, paused: false, reset: true, });
     }
 
     switch (type) {
-      case 'small':
-        this.setState({ boardWidth: 7, boardWidth: 7, start: false, });
+      case 'Small':
+        this.setState({ boardWidth: 7, boardHeight: 7, start: false, boardSize: type, });
         break;
-      case 'medium':
-        this.setState({ boardWidth: 25, boardHeight: 20, start: false, });
+      case 'Medium':
+        this.setState({ boardWidth: 25, boardHeight: 20, start: false, boardSize: type, });
         break;
-      case 'large':
-        this.setState({ boardWidth: 50, boardHeight: 20, start: false, });
+      case 'Large':
+        this.setState({ boardWidth: 40, boardHeight: 30, start: false, boardSize: type, });
         break;
-      case 'slow':
+      case 'Slow':
         this.setState({ speed: 1000, start: false, });
         break;
-      case 'moderate':
+      case 'Moderate':
         this.setState({ speed: 600, start: false, });
         break;
-      case 'fast':
+      case 'Fast':
         this.setState({ speed: 300, start: false, });
         break;
     }
-
-    this.setState(obj);
   };
 
   setNumber = (num) => {
@@ -73,21 +66,21 @@ class App extends Component {
         start={this.state.start} reset={this.state.reset} paused={this.state.paused}/>
         <div className="buttonGroup">
           <Label content="Game controls"/>
-          <Button purp={'start'} clicker={this.clicked}/>
-          <Button purp={'paused'} clicker={this.clicked}/>
-          <Button purp={'reset'} clicker={this.clicked}/>
+          <Button purp={'Start'} clicker={this.clicked}/>
+          <Button purp={'Pause'} clicker={this.clicked}/>
+          <Button purp={'Reset'} clicker={this.clicked}/>
         </div>
         <div className="buttonGroup">
           <Label content="Game speed"/>
-          <Button purp={'slow'} clicker={this.clicked}/>
-          <Button purp={'moderate'} clicker={this.clicked}/>
-          <Button purp={'fast'} clicker={this.clicked}/>
+          <Button purp={'Slow'} clicker={this.clicked}/>
+          <Button purp={'Moderate'} clicker={this.clicked}/>
+          <Button purp={'Fast'} clicker={this.clicked}/>
         </div>
         <div className="buttonGroup">
           <Label content="Board size"/>
-          <Button purp={'small'} clicker={this.clicked}/>
-          <Button purp={'medium'} clicker={this.clicked}/>
-          <Button purp={'large'} clicker={this.clicked}/>
+          <Button purp={'Small'} clicker={this.clicked}/>
+          <Button purp={'Medium'} clicker={this.clicked}/>
+          <Button purp={'Large'} clicker={this.clicked}/>
         </div>
         <div className="buttonGroup">
           <Input placeholder={'Number live cells'} submitter={this.setNumber}/>
